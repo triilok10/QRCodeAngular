c119e8010476ac5150402dd48822a2b55d082c96
triilok10 <ittiku3@gmail.com>
2025-08-28 20:48:21 +0530

    Created required Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../Component/home-component/home-component';
+import { PageNotFoundComponent } from '../Component/page-not-found-component/page-not-found-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  //if path doesnt match any of the above, redirect to Page Not Found component
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
